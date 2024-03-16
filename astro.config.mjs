28a718fdaf84f7b02a22b5baf4e941d1c3b93991
Kalungi Moses <mdrewj@live.com>
2024-03-16 14:55:36 +0300

    Initial deposit: Test deploy

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import { remarkReadingTime } from "./src/remark-reading-time.mjs";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
+});
